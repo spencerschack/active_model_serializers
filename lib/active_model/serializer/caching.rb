77b9f2f2e3f596e061b49c3fdce730168745b389
Spencer Schack <scs1818@gmail.com>
2013-08-22 00:25:28 -0700

    also cache as_json and serializable_hash calls

--- lib/active_model/serializer/caching.rb
+++ lib/active_model/serializer/caching.rb
@@ -2,28 +2,33 @@ module ActiveModel
   class Serializer
     module Caching
       def to_json(*args)
-        if caching_enabled?
-          key = expand_cache_key([self.class.to_s.underscore, cache_key, 'to-json'])
-          cache.fetch key do
-            super
-          end
-        else
-          super
-        end
+        cache_method('to-json') { super }
       end
 
       def serialize(*args)
-        if caching_enabled?
-          key = expand_cache_key([self.class.to_s.underscore, cache_key, 'serialize'])
-          cache.fetch key do
-            serialize_object
-          end
-        else
-          serialize_object
-        end
+        cache_method('serialize') { serialize_object }
+      end
+
+      def as_json(*args)
+        cache_method('as-json') { super }
+      end
+
+      def serializable_hash(*args)
+        cache_method('serializable-hash') { super }
       end
 
       private
+
+      def cache_method key
+        if caching_enabled?
+          key = expand_cache_key([self.class.to_s.underscore, cache_key, key])
+          cache.fetch key do
+            yield
+          end
+        else
+          yield
+        end
+      end
 
       def caching_enabled?
         perform_caching && cache && respond_to?(:cache_key)
